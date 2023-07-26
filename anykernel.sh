a8e15f59ae1442b910d498c8ca57d45b5345c343
ZRCrystal <91072032+ZRbyGit@users.noreply.github.com>
2023-07-26 22:41:04 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=xz1Kernel by charlestai
+kernel.string=KernelSU by ZRCrystal
 do.devicecheck=0
 do.modules=0
 do.systemless=1
